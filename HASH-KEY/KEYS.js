013354d173449777452cc5d2f6c48d720f802726
chrishant <chrishantberwal@gmail.com>
2026-03-06 14:10:45 +0530

    Create KEYS.js

---
+++ HASH-KEY/KEYS.js
@@ -0,0 +1,28 @@
+{
+  "keys": [
+    {
+      "hash": "b8c7a5d7a54cbb9b3e2b7b9c0a4f7d4a2b5a7c1e4d9f8b3a1c2e7f4d8a9b6c1",
+      "user": "Developer",
+      "expiry": "2026-12-31",
+      "active": true
+    },
+    {
+      "hash": "7d3c2a9f1b6e8d4c5a7f9e2b1d3c4a5e6f8b9c7a1d2e3f4c5b6a7d8e9f1a2b3",
+      "user": "QA Tester",
+      "expiry": "2026-08-01",
+      "active": true
+    },
+    {
+      "hash": "a9d4c5b7e1f3a6d8c9b2e4f7a5c6d1b3e8f9a2c4d7e6b5a1c3f8d9e2a7b6c4",
+      "user": "Store Team",
+      "expiry": "2026-05-30",
+      "active": true
+    },
+    {
+      "hash": "e2f4a7c9d3b6a1e5c8f2d4b7a9c1e6f3b5d7a8c2e4f9a1b6c3d5e7f8a2c4b9",
+      "user": "Old Key",
+      "expiry": "2024-12-31",
+      "active": false
+    }
+  ]
+}
