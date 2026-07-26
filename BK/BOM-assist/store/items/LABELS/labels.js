16bf8744bf8054cc296824fdcd2262600369275b
chrishant <chrishantberwal@gmail.com>
2026-07-26 19:28:58 +0530

    Add labels configuration in labels.js

---
+++ BK/BOM-assist/store/items/LABELS/labels.js
@@ -0,0 +1,32 @@
+{
+    "RI": {
+
+        "brands": [
+            "SOME WHERE TO GO",
+            "LOUNGE LOFT"
+        ],
+
+        "labels": {
+
+            "main_label": {
+                "prefix": "MAIN LABEL"
+            },
+
+            "hang_tag": {
+                "prefix": "HANG TAG"
+            },
+
+            "wash_care": {
+                "item_name": "WASH CARE LABEL RI"
+            },
+
+            "size_label": {
+                "prefix": "SIZE LABEL"
+            },
+
+            "coo_label": {
+                "prefix": "COO LABEL"
+            }
+        }
+    }
+}
